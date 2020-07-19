d7f2e5b5d4b64b98f4748ba58d24ffa765f70f55
nmcanall <nmcanall@citadel.edu>
2020-07-19 07:03:22 -0700

    build package and instal inquirer npm

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
-
+    "1) What is your project title?",
+    "2) Describe your project.",
+    "3) How do you instal your project?",
+    "4) What are the steps to use your project?",
+    "5) Give some test cases to run in your project.",
+    "6) What type of license did you use?",
+    "7) What is your GitHub username?",
+    "8) What is your email address?"
 ];
 
 // function to write README file
